05df22038b9abf7115f1e58fd0c2e5311bf3bfc2
mackwick <mackenziewicker@gmail.com>
2023-11-19 14:07:06 -0500

    solved disemvoweling trolls

--- solutions.js
+++ solutions.js
@@ -19,23 +19,32 @@ console.log(setAlarm(true, false))
 // COUNT ODD NUMBERS
 //********************************* */
 
-
 const oddNumberCount = (num) => {
-    let arr = []
-   for (let i = num; i>0; i--) {
+    let arr = [];
+    for (let i = num; i>0; i--) {
     if (i != num && i % 2 != 0) {
-        arr.push(i)
+        arr.push(i);
     } 
    } 
-   return arr.length
+   return arr.length;
 }
-console.log(oddNumberCount(20))
+console.log(oddNumberCount(9))
 
 //********************************* */
 // DISEMVOWELING TROLLS
 //********************************* */
 
+const trollsBeGone = (sentence) => {
+    let newSentence = "";
+    let letters = sentence.split('')
+    for (let letter of letters) {
+        if (letter.toLowerCase() !== "a" && letter.toLowerCase() !== "e" && letter.toLowerCase() !== "i" && letter.toLowerCase() !== "o" && letter.toLowerCase() !== "u") {
+           newSentence = newSentence + `${letter}`;
+        } 
+    } return newSentence
+}
 
+console.log(trollsBeGone("This website is for losers LOL"))
 
 //********************************* */
 // BANK ACCOUNT SUMMARY
